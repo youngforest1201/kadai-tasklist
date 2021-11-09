412daf2c41a2ce88a4819391adad7dc96e2e33f6
keiichi.wakamori <nrj02091@nifty.com>
2021-11-09 15:38:48 +0000

    add column

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -49,11 +49,13 @@ class TasksController extends Controller
     {
         // バリデーション
         $request->validate([
+            'status' => 'required|max:10',   // 追加
             'content' => 'required|max:255',
         ]);
 
         // メッセージを作成
         $task = new Task;
+        $task->status = $request->status;    // 追加
         $task->content = $request->content;
         $task->save();
 
@@ -106,12 +108,14 @@ class TasksController extends Controller
 
         // バリデーション
         $request->validate([
+            'status' => 'required|max:10',   // 追加
             'content' => 'required|max:255',
         ]);
 
         // idの値でメッセージを検索して取得
         $task = Task::findOrFail($id);
         // メッセージを更新
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
 

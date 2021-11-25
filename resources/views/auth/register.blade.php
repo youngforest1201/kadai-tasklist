dc1b2636262bbf0a07aff9bfab66ff6ffca13065
keiichi.wakamori <nrj02091@nifty.com>
2021-11-25 04:10:39 +0000

    user registration

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="text-center">
+        <h1>ユーザ新規登録</h1>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-6 offset-sm-3">
+
+            {!! Form::open(['route' => 'signup.post']) !!}
+                <div class="form-group">
+                    {!! Form::label('name', 'お名前') !!}
+                    {!! Form::text('name', null, ['class' => 'form-control']) !!}
+                </div>
+
+                <div class="form-group">
+                    {!! Form::label('email', 'Email') !!}
+                    {!! Form::email('email', null, ['class' => 'form-control']) !!}
+                </div>
+
+                <div class="form-group">
+                    {!! Form::label('password', 'パスワード') !!}
+                    {!! Form::password('password', ['class' => 'form-control']) !!}
+                </div>
+
+                <div class="form-group">
+                    {!! Form::label('password_confirmation', 'パスワード再入力') !!}
+                    {!! Form::password('password_confirmation', ['class' => 'form-control']) !!}
+                </div>
+
+                {!! Form::submit('登録', ['class' => 'btn btn-primary btn-block']) !!}
+            {!! Form::close() !!}
+        </div>
+    </div>
+@endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,8 @@
     <div class="center jumbotron">
         <div class="text-center">
             <h1>Welcome to the tasks</h1>
+            {{-- ユーザ登録ページへのリンク --}}
+            {!! link_to_route('signup.get', '新登録!', [], ['class' => 'btn btn-lg btn-primary']) !!}
         </div>
     </div>
 @endsection
